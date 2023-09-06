f5bd2a3482b310ce3dda9d5a28723446ae982e18
GonzoTrickster <lancelotserrano.oleriana@bicol-u.edu.ph>
2023-09-07 01:58:49 +0800

    Created Simple CRUD

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Favorite Karaoke Lists</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" /> -->
+</head>
+<body>
+    
+<div class="container">
+    @yield('content')
+</div>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+ Route::get('/', function () {
+     return view('products/index');
+ });
+
+Route::resource('products', ProductController::class);
